8b7201bb600333a3a55a5d2e958dd0fadb7f9c69
Tieu-Philippe KHIM <khim.tieu.philippe@gmail.com>
2018-10-08 09:59:13 +0200

    chore: remove comments

--- lib/middleware/inbound.middleware.ts
+++ lib/middleware/inbound.middleware.ts
@@ -10,11 +10,6 @@ export class InboundMiddleware implements NestMiddleware {
     @InjectCounterMetric('http_requests_in') private readonly _counter: Counter,
   ) {}
 
-  // onModuleInit() {
-    // console.log(`InboundMiddleware::onModuleInit()`);
-    // this._counter = this._moduleRef.get(getMetricToken(`Counter`, `http_requests_in`))
-  // }
-
   resolve(): MiddlewareFunction {
     return (req, res, next) => {
       console.log(`InboundMiddleware::resolve() - ${req.baseUrl}`);
