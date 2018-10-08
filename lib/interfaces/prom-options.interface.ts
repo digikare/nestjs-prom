4ee7d8c2a7bb8074937dc546241ea5fce55f4580
Tieu-Philippe KHIM <khim.tieu.philippe@gmail.com>
2018-10-08 10:43:27 +0200

    chore: adding comment label

--- lib/interfaces/prom-options.interface.ts
+++ lib/interfaces/prom-options.interface.ts
@@ -2,8 +2,21 @@
 export interface PromModuleOptions {
   [key: string]: any;
 
+  /**
+   * Enable default metrics
+   * Under the hood, that call collectDefaultMetrics()
+   */
   withDefaultsMetrics?: boolean;
+
+  /**
+   * Enable internal controller to expose /metrics
+   * Caution: If you have a global prefix, don't forget to prefix it in prom
+   */
   withDefaultController?: boolean;
+
+  /**
+   * Create automatically http_requests_in counter
+   */
   useHttpCounterMiddleware?: boolean;
 
   registryName?: string;
