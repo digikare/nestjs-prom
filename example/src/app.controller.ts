fb01fe9d6bdf7f7d526b8dbc4e6df4dd81256a19
Tieu-Philippe KHIM <khim.tieu.philippe@gmail.com>
2018-10-08 09:59:39 +0200

    chore: adding test route on example for shared counter

--- example/src/app.controller.ts
+++ example/src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Get, Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CounterMetric, InjectCounterMetric } from '../../lib';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @InjectCounterMetric('index_counter') private readonly _counterMetric: CounterMetric,
+  ) {}
 
   @Get()
   root(): string {
     return this.appService.root();
+  }
+
+  @Get('test')
+  test(): string {
+    this._counterMetric.inc(1, new Date());
+    return 'test';
   }
 }
