9aa64486ba3868c3f2c55be9361d79ef3a8752b8
Michael Yankelev <myankelev@gmail.com>
2019-07-09 23:29:09 +0200

    update to nestjs6

--- lib/middleware/inbound.middleware.ts
+++ lib/middleware/inbound.middleware.ts
@@ -1,37 +1,33 @@
-import { Injectable, NestMiddleware, MiddlewareFunction, OnModuleInit } from "@nestjs/common";
+import { Injectable, NestMiddleware } from "@nestjs/common";
 import { Counter } from "prom-client";
-import { InjectCounterMetric, getMetricToken } from "../common";
-import { ModuleRef } from "@nestjs/core";
+import { InjectCounterMetric } from "../common";
 
 @Injectable()
 export class InboundMiddleware implements NestMiddleware {
 
   constructor(
     @InjectCounterMetric('http_requests_total') private readonly _counter: Counter,
-  ) {}
+  ) { }
 
-  resolve(): MiddlewareFunction {
-    return (req, res, next) => {
+  use(req: Request, res: Response, next: Function) {
+    const url = req.url;
+    const method = req.method;
 
-      const url = req.baseUrl;
-      const method = req.method;
-
-      // ignore favicon
-      if (url == '/favicon.ico') {
-        next();
-        return ;
-      }
-
-      // ignore metrics itself
-      // TODO: need improvment to check correctly our current controller
-      if (url.match(/\/metrics(\?.*?)?$/)) {
-        next();
-        return ;
-      }
-
-      this._counter.labels(method).inc(1, new Date());
-
+    // ignore favicon
+    if (url == '/favicon.ico') {
       next();
+      return;
     }
+
+    // ignore metrics itself
+    // TODO: need improvment to check correctly our current controller
+    if (url.match(/\/metrics(\?.*?)?$/)) {
+      next();
+      return;
+    }
+
+    this._counter.labels(method).inc(1, new Date());
+
+    next();
   }
 }
